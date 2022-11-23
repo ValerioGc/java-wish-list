dffa313f1b076d3c3efa78bd329924bb1822eee0
ValerioGc <valerio.gc1@gmail.com>
2022-11-23 11:48:56 +0100

    Avanzamento esercizio

--- src/org/generation/italy/christmas/Main.java
+++ src/org/generation/italy/christmas/Main.java
@@ -1,9 +1,31 @@
 package org.generation.italy.christmas;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
-	public Main() {
-		// TODO Auto-generated constructor stub
-	}
+	public static void main(String[] args) {
 
+		List<String> wishList = new ArrayList<>();
+		Scanner scn = new Scanner(System.in);
+
+		
+		
+		System.out.println("Inserisci desiderio nella wishlist:");
+		String cWish = scn.nextLine();		
+		wishList.add(cWish);
+		
+
+		
+		System.out.println("Lunghezza lista: " + wishList.size());
+		
+		for (String wish : wishList) {
+			System.out.println(wish);
+		}
+		wishList.add("Vuoi aggiungere altro alla wishlist? (1 - si | 2 - no");
+		String uChoice = scn.nextLine();	
+		scn.close();
+	}
 }
