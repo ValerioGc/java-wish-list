ceb12688ae0dfef94027a0cb2da9630f087a971e
ValerioGc <valerio.gc1@gmail.com>
2022-11-23 11:52:45 +0100

    Avanzamento esercizio

--- src/org/generation/italy/christmas/Main.java
+++ src/org/generation/italy/christmas/Main.java
@@ -2,6 +2,7 @@ package org.generation.italy.christmas;
 
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
@@ -10,22 +11,27 @@ public class Main {
 
 		List<String> wishList = new ArrayList<>();
 		Scanner scn = new Scanner(System.in);
-
+		int uChoice = 0;
 		
+		while (uChoice != 2){
+			
+			System.out.println("Inserisci desiderio nella wishlist:");
+			String cWish = scn.nextLine();		
+			wishList.add(cWish);
+			
 		
-		System.out.println("Inserisci desiderio nella wishlist:");
-		String cWish = scn.nextLine();		
-		wishList.add(cWish);
+			Collections.sort(wishList);  
+			
+			for (String wish : wishList) {
+				System.out.println("Desideriod: " + wish);
+			}
+			
+			System.out.println("Lunghezza lista: " + wishList.size() + " elementi");
+			
+			wishList.add("Vuoi aggiungere altro alla wishlist? (1 - si | 2 - no");
+			uChoice = scn.nextInt();	
 		
-
-		
-		System.out.println("Lunghezza lista: " + wishList.size());
-		
-		for (String wish : wishList) {
-			System.out.println(wish);
 		}
-		wishList.add("Vuoi aggiungere altro alla wishlist? (1 - si | 2 - no");
-		String uChoice = scn.nextLine();	
-		scn.close();
+	scn.close();
 	}
 }
